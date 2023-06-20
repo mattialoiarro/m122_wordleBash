a48104a045f042ec80289c9c46b489beff5fb9cd
Maestro <mattia.loiarro@hotmail.com>
2023-06-20 13:20:52 +0200

    Is letter in word works
    I have tested if the letter is present inside the word.

--- wordle.sh
+++ wordle.sh
@@ -5,13 +5,29 @@ max_attempts=5
 result_file="ergebnis.txt"
 
 # Choose a random word from the file
-get_secret_word() {
+getSecretWord() {
     local words=($(cat "$word_file"))
     local num_words=${#words[@]}
     local random_index=$((RANDOM % num_words))
     echo "${words[$random_index]}"
 }
 
+isLetterInWord(){
+    local letter=$1
+    local secretWord=$2
 
-secret_word=$(get_secret_word)
-echo "Secret word: $secret_word"
+    if [[ $secretWord == *"$letter"* ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+letter="a"
+word="apple"
+
+if isLetterInWord "$letter" "$word"; then
+    echo "$letter is present in $word"
+else
+    echo "$letter is not present in $word"
+fi
